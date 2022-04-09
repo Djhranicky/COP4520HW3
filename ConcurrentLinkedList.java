8ca17f8ffd7e5f9bc9c80d1d10a6ff89f3afbf8a
David Hranicky <djh30000@gmail.com>
2022-04-09 12:58:55 -0400

    Implemented lockfree linked list from the textbook

---
+++ ConcurrentLinkedList.java
@@ -0,0 +1,106 @@
+import java.util.concurrent.atomic.AtomicMarkableReference;
+
+public class ConcurrentLinkedList{
+    private Node head;
+
+    public ConcurrentLinkedList(){
+        this.head = new Node(Integer.MIN_VALUE);
+        this.head.next = null;
+    }
+
+    public ConcurrentLinkedList(int value){
+        this.head = new Node(Integer.MIN_VALUE);
+        Node next = new Node(value);
+        this.head.next = new AtomicMarkableReference<Node>(next, false);
+    }
+
+    public Window find(Node head, int key){
+        Node pred = null, curr = null, succ = null;
+        boolean[] marked = {false};
+        boolean snip;
+        retry: while(true){
+            pred = head;
+            curr = pred.next.getReference();
+            while(true){
+                succ = curr.next.get(marked);
+                while(marked[0]){
+                    snip = pred.next.compareAndSet(curr, succ, false, false);
+                    if(!snip) continue retry;
+                    curr = succ;
+                    succ = curr.next.get(marked);
+                }
+                if(curr.key >= key){
+                    return new Window(pred, curr);
+                }
+                pred = curr;
+                curr = succ;
+            }
+        }
+    }
+
+    public boolean contains(int key){
+        boolean marked;
+        Node curr = this.head;
+        while(curr.key < key){
+            curr = curr.next.getReference();
+        }
+        marked = curr.next.isMarked();
+        return (curr.key == key && !marked);
+    }
+
+    public boolean add(int key){
+        while(true){
+            Window window = find(this.head, key);
+            Node pred = window.pred, curr = window.curr;
+            if (curr.key == key){
+                return false;
+            } else {
+                Node node = new Node(key);
+                node.next = new AtomicMarkableReference<Node>(curr, false);
+                if(pred.next.compareAndSet(curr, node, false, false)){
+                    return true;
+                }
+            }
+        }
+    }
+
+    public boolean remove(int key){
+        boolean snip;
+        while(true){
+            Window window = find(this.head, key);
+            Node pred = window.pred, curr = window.curr;
+            if(curr.key != key){
+                return false;
+            } else {
+                Node succ = curr.next.getReference();
+                snip = curr.next.compareAndSet(succ, succ, false, true);
+                if(!snip) continue;
+                pred.next.compareAndSet(curr, succ, false, false);
+                return true;
+            }
+        }
+    }
+
+    private class Node{
+        int key;
+        AtomicMarkableReference<Node> next;
+
+        public Node(int key){
+            this(key, null);
+        }
+
+        public Node(int key, AtomicMarkableReference<Node> next){
+            this.key = key;
+            this.next = next;
+        }
+    }
+
+    private class Window{
+        Node pred;
+        Node curr;
+        public Window(Node pred, Node curr){
+            this.pred = pred;
+            this.curr = curr;
+        }
+    }
+}
