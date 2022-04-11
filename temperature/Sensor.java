51643bb906080d5e592a10eec93d51e98dbf9761
David Hranicky <djh30000@gmail.com>
2022-04-11 15:23:21 -0400

    Sensors run independent of main thread

--- temperature/Sensor.java
+++ temperature/Sensor.java
@@ -6,20 +6,33 @@ public class Sensor implements Runnable {
     
     double[][] data;
     int threadNum;
+    int hours;
+    boolean[][] finished;
     Random r;
     final int RANGE = 170;
     final int OFFSET = -100;
 
-    public Sensor(double[][] data, int threadNum){
+    public Sensor(double[][] data, int threadNum, int hours, boolean[][] finished){
         this.data = data;
         this.threadNum = threadNum;
+        this.hours = hours;
+        this.finished = finished;
     }
 
     @Override
     public void run(){
-        for(int i = 0; i < 60; i++){
-            r = new Random();
-            data[i][threadNum] = ((r.nextDouble() * RANGE) + OFFSET);
+        for(int hour = 0; hour < hours; hour++){
+            for(int i = 0; i < 60; i++){
+                // Simulated minute
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                r = new Random();
+                data[i][threadNum] = ((r.nextDouble() * RANGE) + OFFSET);            
+            }
+            finished[hour][threadNum] = true;
         }
     }
 }
