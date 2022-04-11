616c5f1f9319b6e0db0fd3012352257c235dadb8
David Hranicky <djh30000@gmail.com>
2022-04-11 15:24:37 -0400

    Create report once sensors collect required data

--- temperature/Temperature.java
+++ temperature/Temperature.java
@@ -14,33 +14,53 @@ public class Temperature {
         double[][] extremes;
         double[] highest, lowest;
         Double maxInterval;
+        boolean[][] finished;
         
         System.out.println("How many hours?");
         hours = sc.nextInt();
 
-        for(int hour = 0; hour < hours; hour++){
-            data = new double[60][];
-            extremes = new double[60][];
-            highest = new double[5];
-            lowest = new double[5];
-            maxInterval = Double.MIN_VALUE;
-            for(int i = 0; i < 60; i++){
-                data[i] = new double[8];
-                extremes[i] = new double[2];
-            }
+        data = new double[60][];
+        extremes = new double[60][];
+        highest = new double[5];
+        lowest = new double[5];
+        maxInterval = Double.MIN_VALUE;
+        for(int i = 0; i < 60; i++){
+            data[i] = new double[8];
+            extremes[i] = new double[2];
+        }
 
-            Thread[] threads = new Thread[NUM_THREADS];
-            for(int i = 0; i < NUM_THREADS; i++){
-                threads[i] = new Thread(new Sensor(data, i));
-                threads[i].start();
+        finished = new boolean[hours][NUM_THREADS];
+        Thread[] threads = new Thread[NUM_THREADS];
+        for(int i = 0; i < NUM_THREADS; i++){
+            threads[i] = new Thread(new Sensor(data, i, hours, finished));
+            threads[i].start();
+        }
+
+        int hour = 0;
+
+        // Run while at least one sensor is still working
+        while(!finished[hours-1][0] || 
+        !finished[hours-1][1] ||
+        !finished[hours-1][2] ||
+        !finished[hours-1][3] ||
+        !finished[hours-1][4] ||
+        !finished[hours-1][5] ||
+        !finished[hours-1][6] ||
+        !finished[hours-1][7]){
+
+            // Wait for the sensors to gather the data
+            while(!finished[hour][0] ||
+            !finished[hour][1] ||
+            !finished[hour][2] ||
+            !finished[hour][3] ||
+            !finished[hour][4] ||
+            !finished[hour][5] ||
+            !finished[hour][6] ||
+            !finished[hour][7]){
+                System.out.print("");
             }
-            for(int i = 0; i < NUM_THREADS; i++){
-                try{
-                    threads[i].join();
-                } catch(Exception e){
-                    e.printStackTrace();
-                }
-            }
+            // Data gethered, calulate the report
+            long start = System.currentTimeMillis();
 
             for(int i = 0; i < 60; i++){
                 double[] minute = data[i];
@@ -71,7 +91,30 @@ public class Temperature {
             System.out.println("The highest 5 temperatures for hour "+(hour+1)+" were "+Arrays.toString(highest));
             System.out.println("The lowest 5 temperatures for hour "+(hour+1)+" were "+Arrays.toString(lowest));
             System.out.println("The largest temperature change over a 10 minute period was "+maxInterval);
-            System.out.println("\n");
+
+            extremes = new double[60][];
+            highest = new double[5];
+            lowest = new double[5];
+            maxInterval = Double.MIN_VALUE;
+            for(int i = 0; i < 60; i++){
+                data[i] = new double[8];
+                extremes[i] = new double[2];
+            }
+
+            long end = System.currentTimeMillis();
+
+            System.out.println("The report was generated in "+(end-start)+" milliseconds");
+            System.out.println("");
+            
+            hour++;
+        }
+        for(int i = 0; i < NUM_THREADS; i++){
+            try {
+                threads[i].join();
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
         }
         sc.close();
     }
